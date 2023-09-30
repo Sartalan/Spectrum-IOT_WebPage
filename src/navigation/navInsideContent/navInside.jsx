20d1be4683d378514e9e2110beccbaffd124ce50
Sarti <elpinguinoalan@gmail.com>
2023-09-30 20:38:12 -0300

    Nav Feature 0.5v ! Scroll Icons Add

--- src/navigation/navInsideContent/navInside.jsx
+++ src/navigation/navInsideContent/navInside.jsx
@@ -1,44 +1,47 @@
+{
+  /*
+    Componentes Internos de la Barra de Navegación Interna 
+    que corresponderán a la movilidad de cada uno de los MiniBots
+  */
+}
+
+
 import './navInside.css'
+
+import controlIcon from './src_inside/example.svg'
 
 export function Inside(){
  return (
     <div className='inside-nav'>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
-      <h2>asdaw</h2>
+
+      <InsideNavComponent 
+        controllerIconComponent={controlIcon}
+      />
+        <InsideNavComponent 
+        controllerIconComponent={controlIcon}
+      />
+ 
+      <InsideNavComponent 
+        controllerIconComponent={controlIcon}
+      />
+      
+      <InsideNavComponent 
+        controllerIconComponent={controlIcon}
+      />
+ 
+ 
+      
       
       
       
     </div>
  )
 } 
+
+export function InsideNavComponent({controllerIconComponent}){
+  return (
+    <div className="inside-component">
+      <img className='icon-component' src={controllerIconComponent} alt="" />  
+    </div>
+  )
+}
